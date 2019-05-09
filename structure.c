60010317b0c207150a7c00cc674c18bd53f1d244
Lilian Billod <lilian.billod@grenoble-inp.org>
2019-05-09 10:25:27 +0200

    Ajout des tas

--- structure.c
+++ structure.c
@@ -21,6 +21,13 @@ void affiche_liste(L_ARC l) {
     }
 }
 
+void affiche_liste_sommet(L_SOMMET l) {
+    L_SOMMET p=NULL;
+    for (p=l; !liste_vide_s(p); p=p->suiv) {
+        affiche_sommet(p->val);
+    }
+}
+
 void affiche_arc(T_ARC a) {
     printf("Arrivee: %d, cout: %lf\n",a.arrivee,a.cout);
 }
@@ -32,7 +39,14 @@ L_ARC ajout_tete(T_ARC t,L_ARC l) {
     return p;
 }
 
+L_SOMMET ajout_tete_s(T_SOMMET t,L_SOMMET l) {
+    L_SOMMET p=creer_liste_s();
+    if (!(liste_vide_s(p=calloc( 1, sizeof (*p)))))  { p->val=t; p->suiv=l; }
+    return p;
+}
+
 L_ARC creer_liste() {return NULL;}
+L_SOMMET creer_liste_s() {return NULL;}
 
 L_ARC supprimer_tete(L_ARC l) {
     if (liste_vide(l)) {
@@ -44,7 +58,18 @@ L_ARC supprimer_tete(L_ARC l) {
     return l;
 }
 
+L_SOMMET supprimer_tete_s(L_SOMMET l) {
+    if (liste_vide_s(l)) {
+        return NULL;
+    }
+    L_SOMMET p=l;
+    l=l->suiv;
+    free(p);
+    return l;
+}
+
 int liste_vide(L_ARC l) {return (l==NULL);}
+int liste_vide(L_SOMMET l) {return (l==NULL);}
 
 //Supprimer le nième élément de la liste (on compte à partir de 0)
 L_ARC supprimen(int n,L_ARC l) {
@@ -64,7 +89,27 @@ L_ARC supprimen(int n,L_ARC l) {
         return l;
     }
     k->suiv=supprimer_tete(k->suiv);
-    return l;void affiche_graphe(GRAPHE g);
+    return l;
+}
+
+L_SOMMET supprimen_s(int n,L_SOMMET l) {
+    int i=0;
+    if (liste_vide_s(l)) {
+        return NULL;
+    }
+    if (n==0) {
+        return supprimer_tete_s(l);
+    }
+    L_SOMMET k=l;
+    while ((!liste_vide_s(k))&&(i<(n-1))) {
+      k=k->suiv;
+      i+=1;
+    }
+    if (liste_vide_s(k)) {
+        return l;
+    }
+    k->suiv=supprimer_tete_s(k->suiv);
+    return l;
 }
 
 //libere la liste
@@ -74,6 +119,14 @@ L_ARC liberer_liste(L_ARC l) {
     }
   L_ARC k=supprimer_tete(l);
   return liberer_liste(k);
+}
+
+L_SOMMET liberer_liste_s(L_SOMMET l) {
+    if (liste_vide_s(l)) {
+        return l;
+    }
+  L_SOMMET k=supprimer_tete_s(l);
+  return liberer_liste_s(k);
 }
 
 //renvoie 1 si a est dans l, 0 sinon
@@ -101,4 +154,82 @@ L_ARC recherche_liste(T_ARC a,L_ARC l) {
 //renvoie 1 si a et b sont identiques 0 sinon
 int compare_arc(T_ARC a,T_ARC b) {
     return ((a.arrivee==b.arrivee)&&(a.cout==b.cout));
+}
+
+//Affiche un tas de taille n
+void afficheTas(int* tas,int n) {
+    int i;
+    if (n!=0) {
+        printf("[%d",tas[0]);
+        for (i=1;i<n;i++) {
+            printf(",%d",tas[i]);
+        }
+        printf("]");
+    }
+    else {
+        printf("[]");
+    }
+}
+
+//Echange les éléments i et j du tableau tas
+void echangeTas(int* tas,int i,int j) {
+    int tmp;
+    tmp=tas[i];
+    tas[i]=tas[j];
+    tas[j]=tmp;
+}
+
+//Ajoute le nombre d'indice i au tas
+void augmenteTas(int* tas,double* listeCout, int i) {
+    int k=i;
+    while ((k!=0) && (listeCout[tas[k]]>listeCout[tas[(k-1)/2]])) {
+        echangeTas(tas,k,(k-1)/2);
+        k=(k-1)/2;
+    }
+}
+
+//Construit un tas à partir d'un tableau de n éléments
+void constructionTas(double* tas, int n) {
+    int i;
+    for (i=0;i<n;i++) {
+        augmenteTas(tas,i);
+    }
+}
+
+//Permet de placer la racine à sa place dans le tas (qui est limité à i-1)
+void descendreTas(int* tas,double* listeCout, int i) {
+    int k=0;
+    int maxk;
+    double max;
+    while ((((2*(k+1))<i)&&(listeCout[tas[k]]<listeCout[tas[2*(k+1)]]))||((((2*(k+1))-1)<i)&&(listeCout[tas[k]]<listeCout[tas[(2*(k+1))-1]]))) {
+        if ((2*(k+1))>=i) {
+            echangeTas(tas,k,2*(k+1)-1);
+            k=2*(k+1)-1;
+        }
+        else {
+            if (listeCout[tas[2*(k+1)-1]]>listeCout[tas[2*(k+1)]]) {
+                echangeTas(tas,k,2*(k+1)-1);
+                k=2*(k+1)-1;
+            }
+            else {
+                echangeTas(tas,k,2*(k+1));
+                k=2*(k+1);
+            }
+        }
+    }
+}
+
+//Permet de supprimer la racine d'un tas de taille i-1
+void supprimerTas(double* tas, int i) {
+    echangeTas(tas,i-1,0);
+    descendreTas(tas,i-1);
+}
+
+//Trie le tableau de taille n par le tri Tas
+void triTas(double* tas, int n) {
+    int i;
+    constructionTas(tas,n);
+    for (i=n;i>1;i--) {
+        supprimerTas(tas,i);
+    }
 }
--- structure.h
+++ structure.h
@@ -4,6 +4,7 @@
 typedef struct { int arrivee; double cout; } T_ARC ;
 typedef struct lsucc { T_ARC val; struct lsucc* suiv ; }* L_ARC;
 typedef struct { char* nom; double x,y ; L_ARC voisins;} T_SOMMET ;
+typedef struct _lsucc { T_SOMMET val; struct _lsucc* suiv ; }* L_SOMMET;
 typedef struct {int n;T_SOMMET* tab;} GRAPHE;
 
 
@@ -12,18 +13,34 @@ void affiche_graphe(GRAPHE g);
 void affiche_sommet(T_SOMMET s);
 void affiche_liste(L_ARC l);
 void affiche_arc(T_ARC a);
+void affiche_liste_sommet(L_SOMMET l);
 
 //Fonctions sur les listes
 L_ARC ajout_tete(T_ARC t,L_ARC l);
+L_SOMMET ajout_tete_s(T_SOMMET t,L_SOMMET l);
 L_ARC creer_liste();
+L_SOMMET creer_liste_s();
 L_ARC supprimer_tete(L_ARC l);
+L_SOMMET supprimer_tete_s(L_SOMMET l);
 int liste_vide(L_ARC l);
+int liste_vide(L_SOMMET l);
 L_ARC supprimen(int n,L_ARC l);
+L_SOMMET supprimen_s(int n,L_SOMMET l);
 L_ARC liberer_liste(L_ARC l);
+L_SOMMET liberer_liste_s(L_SOMMET l);
 int in_liste(T_ARC a,L_ARC l);
 L_ARC recherche_liste(T_ARC a,L_ARC l);
 
 //Fonctions sur les arcs
 int compare_arc(T_ARC a,T_ARC b);
+
+//Fonctions sur les Tas
+void afficheTas(int* tas, int n);
+void echangeTas(int* tas,int i,int j);
+void augmenteTas(int* tas,double* listeCout, int i);
+void constructionTas(int* tas, int n);
+void descendreTas(int* tas,double* listeCout, int i);
+void suppresionTas(int* tas,int i);
+void triTas(int* tas,int n);
 
 #endif
